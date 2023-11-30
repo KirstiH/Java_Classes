5784eab8f8386b56186b543ce7ad1503ca43c64b
kiharo <kiharo@jyu.fi>
2023-11-30 14:57:30 +0200

    Interface Teacher

---
+++ Teacher.java
@@ -0,0 +1,12 @@
+package main.java.dev.m3s.programming2.homework3;
+
+/*
+ * interface for classes Teacher and AssistantTeacher
+ * all teachers need to have course information
+ */
+
+public interface Teacher {
+    
+    public abstract String getCourses();
+
+}
